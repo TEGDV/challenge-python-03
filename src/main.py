95b2fc4b2cc579112e6b8ed4b093b28057c88626
Jair Aguilar <apjair97@gmail.com>
2020-07-05 06:16:48 -0500

    [Update] Trying to solve the challenge with the regular expretions lib

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 # Resolve the problem!!
-
+import re
 
 def run():
-    # Start coding here
+   with open('/mnt/c/users/apjai/documents/developer/challenge3_py/src/encoded.txt', 'r', encoding='utf-8') as f:
+    # code
+    message = f.read()
+    real_message = re.findall('[a-z]', message)
+    print(real_message)
 
 
 if __name__ == '__main__':
